c34e3f9a4713a15a3776b4b68c3716966273e74b
Alex Bechmann <alex.bechmann@danfoss.com>
2020-06-30 15:08:44 +0200

    browser tests

--- __tests__/3-browser-tests.tsx
+++ __tests__/3-browser-tests.tsx
@@ -1,27 +1,27 @@
-import { Builder, By, WebDriver } from 'selenium-webdriver';
+import { Builder, By, WebDriver, Key } from 'selenium-webdriver';
 import { expect } from 'chai';
 
 describe('browser tests', () => {
-  const browsers = ['firefox'];
+  const browsers = ['firefox', 'chrome']; // 'internet explorer'
 
   browsers.forEach((browser) => {
     let driver: WebDriver;
 
-    beforeEach(async () => {
+    before(async () => {
       driver = await new Builder().forBrowser(browser).build();
     });
 
-    it('Has correct home page text', async () => {
+    it(`Has correct home page text (${browser})`, async () => {
       driver.navigate().to('https://www.danfoss.com/en/');
       const titleElement = await driver.findElement(By.className('card__title'));
       const text = await titleElement.getText();
       expect(text).to.equal('Danfoss – Engineering Tomorrow');
     });
 
-    it('Shows cookie dialog on first time', async () => {
+    it(`Shows cookie dialog on first time (${browser})`, async () => {
       driver.navigate().to('https://www.danfoss.com/en/');
 
-      const cookiePopupElement = await driver.findElement(By.id('coiOverlay'));
+      let cookiePopupElement = await driver.findElement(By.id('coiOverlay'));
 
       let style = await cookiePopupElement.getAttribute('style');
       expect(style).to.equal('display: flex;');
@@ -33,8 +33,20 @@ describe('browser tests', () => {
       expect(style).to.equal('display: none;');
     });
 
-    afterEach(async () => {
-      await driver.close();
+    // it('First search result is ', async () => {
+    //   driver.navigate().to('https://www.danfoss.com/en/');
+
+    //   const searchInputElement = await driver.findElement(By.id('react-select-2-input'));
+    //   searchInputElement.sendKeys('valve');
+    //   searchInputElement.sendKeys(Key.ENTER);
+
+    //   const resultTitles = await driver.findElements(By.className('tile__text-title'));
+    //   const firstResultText = await resultTitles[0].getText();
+    //   expect(firstResultText).to.equal('Danfoss Dynamic valve™');
+    // });
+
+    after(async () => {
+      await driver?.close();
     });
   });
 });
